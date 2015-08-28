cef2e35cbfb1ce6df13b8ed9010a95fa154e490e
Vanja Sorokin <tsukimizuki69@gmail.com>
2015-08-28 19:19:51 +1000

    Menus, Build Settings
    set up a main menu, and build settings to fit game well.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void StartGame()
+	{
+		Application.LoadLevel("Instructions");
+	}
+
+	void Quit()
+	{
+		Application.Quit();
+	}
+
+	void LoadMainScene()
+	{
+		Application.LoadLevel("MainGameScene");
+	}
+}
